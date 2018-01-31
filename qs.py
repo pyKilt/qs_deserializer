bca300ff5ed8f5a4f167aba19859be730f9a534e
Jesús Germade <jesus@germade.es>
2018-01-31 16:07:40 +0100

    wip serializer

--- qs.py
+++ qs.py
@@ -1,6 +1,14 @@
 # QueryString deserializer
 
-def deserialize():    # write Fibonacci series up to n
-    return 5
+def deserialize(qs):    # write Fibonacci series up to n
+    if( not isinstance(qs, str) ):
+        raise TypeError('should be a string')
+    params = qs.split('&')
+    result = {}
+
+    for param in params:
+        print param
+        
+    return qs.split('&')
 
 __all__ = ['deserialize']
--- tests/basic-test.py
+++ tests/basic-test.py
@@ -8,4 +8,4 @@ def test_answer():
     assert inc(3) == 4
 
 def test_deserialize():
-    assert deserialize() == 5
+    assert deserialize('hola=caracola&adios=caracol&foo=bar') == ['hola=caracola', 'adios=caracol', 'foo=bar']

5c6c499cc899dc925f90e76f1d7acd1798f354be
Jesús Germade <jesus@germade.es>
2018-02-12 13:42:34 +0100

    v0.2.9

--- qserious/__init__.py
+++ qserious/__init__.py
@@ -6,6 +6,6 @@
 
 from .qserious import deserialize, serialize, setKey, bracketsToDots, keysTobrackets
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 
 # __all__ = ['deserialize', 'serialize', 'setKey', 'bracketsToDots', 'keysTobrackets']
